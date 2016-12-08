2f3c79512cb610a17b5855c1ee03afc6867ffc21
yar <yardenack@gmail.com>
2016-12-07 23:46:58 -0800

    chmod to avoid some permission errors

--- sync.sh
+++ sync.sh
@@ -3,4 +3,4 @@
 self=$(readlink -e "$0") || exit 1
 self=$(dirname "${self}") || exit 1
 
-rsync --inplace --delete --out-format="%t %o %f ... %n" --filter=". ${self}/rs-filter" -Phac / "${self}/rs/"
+rsync --inplace --delete --out-format="%t %o %f ... %n" --filter=". ${self}/rs-filter" --chmod=u+w,Du+x -Phac / "${self}/rs/"
